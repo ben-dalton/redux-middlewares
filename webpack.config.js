0b2b8de08426adb69b9853b83624ddf4ae42518f
ben-dalton <benadalton@gmail.com>
2016-04-21 16:49:44 -0400

    add sass compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,49 @@
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const sassLoaders = [
+	'css-loader',
+	'postcss-loader',
+	'sass-loader?indentSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
+]
+
+const config = {
+  entry: {
+    app: ['./src/index.js']
+	},
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+		filename: '[name].js',
+    path: path.join(__dirname, './build'),
+    publicPath: '/build',
   },
   module: {
     loaders: [{
       exclude: /node_modules/,
       loader: 'babel'
-    }]
+			},
+			{
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      }
+    ]
   },
+	plugins: [
+		new ExtractTextPlugin('[name].css')
+	],
+	postcss: [
+		autoprefixer({
+			brosers: ['last 2 versions']
+		})
+	],
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', '.sass'],
+		root: [path.join(__dirname, './src')]
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
   }
 };
+
+module.exports = config;
