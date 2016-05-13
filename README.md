0e9406048d051d793b26bef10639d72a8ff23d48
ben-dalton <benadalton@gmail.com>
2016-05-13 14:46:50 -0400

    apply a custom middleware mimicking redux-promise

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ReduxSimpleStarter
+# Redux Middlewares
 
 ##Getting Started###
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import Async from './middlewares/async';
 import '../stylesheets/styles';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(Async)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
