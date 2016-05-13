4fe01092581b3f8535b10c9de88bdaf491ac2da0
ben-dalton <benadalton@gmail.com>
2016-05-13 14:52:53 -0400

    use placeholder id and add website link

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -8,10 +8,10 @@ class UserList extends Component {
 	}
 	renderUser(user) {
 		return (
-			<div className="card card-block" key={user.name}>
+			<div className="card card-block" key={user.id}>
 				<h4 className="card-title">{user.name}</h4>
 				<p className="card-text">{user.company.name}</p>
-				<a className="btn btn-primary" href={user.website}>Website</a>
+				<a className="btn btn-primary" href={`http://${user.website}`}>Website</a>
 			</div>
 		);
 	}
